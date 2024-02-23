b6ae928e9c2384349c29ffdf0a2654a84ad57fe6
marinni1 <124533330+marinni1@users.noreply.github.com>
2024-02-23 06:11:25 +0000

    final

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add JavaScript below
+ScrollReveal().reveal('.frame1', {delay:1000});
+
+ScrollReveal().reveal('.frame2', {
+    distance: '20px',
+    duration: 1000,
+    origin: 'left', 
+    easing: 'ease-out',
+    reset: true
+  });
+
+  ScrollReveal().reveal('.frame3', {
+    distance: '20px',
+    duration: 1000,
+    origin: 'bottom',
+    easing: 'ease-out',
+    reset: true,
+    delay: 1000 
+});
+
+ScrollReveal().reveal('.frame5', {
+    distance: '20px',
+    duration: 1000,
+    origin: 'right', 
+    easing: 'ease-out',
+    reset: true,
+    delay: 2000 
+  });
+
+  ScrollReveal().reveal('.frame6', {
+    distance: '20px',
+    duration: 1000,
+    origin: 'top', 
+    easing: 'ease-out',
+    reset: true,
+    delay: 1000 
+  });
